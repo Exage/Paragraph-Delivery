53f13b7a37b84edab1fc83bf8f9acb1f08ca1355
Exage <horkavchuk.nik@gmail.com>
2023-11-15 22:45:09 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'the-new-css-reset/css/reset.css'
+import './App.scss'
+
+function App() {
+	return (
+		<div className="App">
+			<h1 className='title'>QQQQQ</h1>
+		</div>	
+	)
+}
+
+export default App
